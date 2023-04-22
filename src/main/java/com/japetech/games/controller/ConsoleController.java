3833dc4fdde25aabfc0f7b26885d5c3008af7b26
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-04-22 14:37:37 -0300

    FINALIZADO!

--- src/main/java/com/japetech/games/controller/ConsoleController.java
+++ src/main/java/com/japetech/games/controller/ConsoleController.java
@@ -4,6 +4,11 @@ package com.japetech.games.controller;
 import com.japetech.games.gamesDtos.ConsoleDto;
 import com.japetech.games.models.ConsoleModel;
 import com.japetech.games.services.ConsoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -12,8 +17,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Tag(name = "Console", description = "API para gerenciamento de consoles")
 @RestController
 @RequestMapping("/console")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ConsoleController {
 
     final ConsoleService consoleService;
@@ -22,20 +29,15 @@ public class ConsoleController {
         this.consoleService = consoleService;
     }
 
+    @Operation(summary = "Lista todas os consoles", description = "Lista todos os consoles")
     @GetMapping("/")
     public ResponseEntity<Object> get(){
         return ResponseEntity.status(HttpStatus.OK).body(consoleService.getAll());
     }
 
-    @PostMapping("/")
-    public ResponseEntity<Object> saveGame(@Valid @RequestBody ConsoleDto consoleDto){
-
-        ConsoleModel consoleModel = new ConsoleModel();
-        BeanUtils.copyProperties(consoleDto, consoleModel);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(consoleService.save(consoleModel));
-    }
-
+    @Operation(summary = "Recupera um console pelo ID", description = "Recupera os dados de um console a partir do seu ID")
+    @ApiResponse(responseCode = "200", description = "Console encontrado com sucesso", content = @Content(schema = @Schema(implementation = ConsoleModel.class)))
+    @ApiResponse(responseCode = "404", description = "Console não encontrado")
     @GetMapping("/{id}")
     public ResponseEntity<ConsoleModel> getGameById(@PathVariable Long id){
         Optional<ConsoleModel> optionalGame = Optional.ofNullable(consoleService.findById(id));
@@ -47,6 +49,19 @@ public class ConsoleController {
         }
     }
 
+    @Operation(summary = "Salva um console", description = "Salva um console")
+    @ApiResponse(responseCode = "201", description = "Console salva com sucesso", content = @Content(schema = @Schema(implementation = ConsoleModel.class)))
+    @PostMapping("/")
+    public ResponseEntity<Object> saveGame(@Valid @RequestBody ConsoleDto consoleDto){
+
+        ConsoleModel consoleModel = new ConsoleModel();
+        BeanUtils.copyProperties(consoleDto, consoleModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(consoleService.save(consoleModel));
+    }
+
+    @Operation(summary = "Exclui um console pelo ID", description = "Exclui um console a partir do seu ID")
+    @ApiResponse(responseCode = "204", description = "Console excluido com sucesso")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGame(@PathVariable Long id){
         consoleService.deleteById(id);
--- src/main/java/com/japetech/games/controller/ConsoleGameController.java
+++ src/main/java/com/japetech/games/controller/ConsoleGameController.java
@@ -3,6 +3,11 @@ package com.japetech.games.controller;
 import com.japetech.games.gamesDtos.ConsoleGameDto;
 import com.japetech.games.models.ConsoleGameModel;
 import com.japetech.games.services.ConsoleGameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -11,8 +16,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Tag(name = "Console Game", description = "API para gerenciamento de consoles games")
 @RestController
 @RequestMapping("/consolegame")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ConsoleGameController {
 
     final ConsoleGameService consoleGameService;
@@ -21,20 +28,15 @@ public class ConsoleGameController {
         this.consoleGameService = consoleGameService;
     }
 
+    @Operation(summary = "Lista todos os console games", description = "Lista todos os console games")
     @GetMapping("/")
     public ResponseEntity<Object> get(){
         return ResponseEntity.status(HttpStatus.OK).body(consoleGameService.getAll());
     }
 
-    @PostMapping("/")
-    public ResponseEntity<Object> saveGame(@Valid @RequestBody ConsoleGameDto consoleGameDto){
-
-        ConsoleGameModel consoleGameModel = new ConsoleGameModel();
-        BeanUtils.copyProperties(consoleGameDto, consoleGameModel);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(consoleGameService.save(consoleGameModel));
-    }
-
+    @Operation(summary = "Recupera um console game pelo ID", description = "Recupera os dados de um console game a partir do seu ID")
+    @ApiResponse(responseCode = "200", description = "Console Game encontrado com sucesso", content = @Content(schema = @Schema(implementation = ConsoleGameModel.class)))
+    @ApiResponse(responseCode = "404", description = "Console Game não encontrado")
     @GetMapping("/{id}")
     public ResponseEntity<ConsoleGameModel> getGameById(@PathVariable Long id){
         Optional<ConsoleGameModel> optionalGame = Optional.ofNullable(consoleGameService.findById(id));
@@ -46,6 +48,19 @@ public class ConsoleGameController {
         }
     }
 
+    @Operation(summary = "Salva o console game", description = "Salva o console game")
+    @ApiResponse(responseCode = "201", description = "Console Game salva com sucesso", content = @Content(schema = @Schema(implementation = ConsoleGameModel.class)))
+    @PostMapping("/")
+    public ResponseEntity<Object> saveGame(@Valid @RequestBody ConsoleGameDto consoleGameDto){
+
+        ConsoleGameModel consoleGameModel = new ConsoleGameModel();
+        BeanUtils.copyProperties(consoleGameDto, consoleGameModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(consoleGameService.save(consoleGameModel));
+    }
+
+    @Operation(summary = "Exclui um console game pelo ID", description = "Exclui um console game a partir do seu ID")
+    @ApiResponse(responseCode = "204", description = "Console Game excluido com sucesso")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGame(@PathVariable Long id){
         consoleGameService.deleteById(id);
